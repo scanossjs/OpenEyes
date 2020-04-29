5dd03e02720a9d9e4752c80a4642430b30fd80f7
Juan M Salamanca <50915362+scanossjs@users.noreply.github.com>
2020-04-29 23:03:05 +0200

    Update index-test.php

--- index-test.php
+++ index-test.php
@@ -3,7 +3,7 @@
  * This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
  */
-// Test
+// Test 2
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii-1.1.5.r2654/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
